25bd318687ae3e580d4c0934ce08732671ba1170
Maria <maria.cilione@gmail.com>
2018-07-06 14:40:07 +0200

    primo commit

---
+++ DemoAppCenter/DemoAppCenter/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DemoAppCenter.Models;
+
+namespace DemoAppCenter.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
